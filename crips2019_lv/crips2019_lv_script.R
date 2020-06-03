5f3368880a26766dbbe5861c6ce3744ae70ba1c9
Nicolas Bressoud <65454893+bresnico@users.noreply.github.com>
2020-06-03 11:01:16 +0200

    ajout crips

---
+++ crips2019_lv/crips2019_lv_script.R
@@ -0,0 +1,14 @@
+# Importation des données pré-nettoyées dans Qualtrics----
+
+
+# Ajustement des noms de variables----
+
+
+# Repérage des données manquantes----
+
+
+# Repérage des données extrêmes----
+
+
+# normalisation des id en minuscule----
+
