9f6486bd53f4aa83ab7191b081e12643e20321b8
Nicolas Bressoud <65454893+bresnico@users.noreply.github.com>
2020-10-03 10:15:28 +0200

    Début analyses AN

---
+++ csvr2020_an/csvr2020_an_script.R
@@ -0,0 +1,7 @@
+# lancement des packages----
+library(tidyverse)
+library(readxl)
+
+# Importation des données brutes (N=155) sous la forme d'une répartition en 4 temps. On a déjà supprimé les ID invalides auparavant (p.ex. "test") en repérant visuellement la structure de chaque ID (forme acceptée de type: abcd01). On a supprimé les colonnes avec infomations méta inutiles de qualtrics.
+
+
