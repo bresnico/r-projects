e59858e3e953e6f8c37fb4a239afc00bd6e201ad
Nicolas Bressoud <65454893+bresnico@users.noreply.github.com>
2021-03-08 11:32:05 +0100

    maj limesurvey

--- sandbox_limesurvey/sandbox_limesurvey_script.R
+++ sandbox_limesurvey/sandbox_limesurvey_script.R
@@ -3,7 +3,7 @@
 library(limer)
 library(tidyverse)
 library(lubridate)
-
+library(rio)
 
 # Setup API details
 options(lime_api = 'https://survey.competences-emotionnelles.ch/admin/remotecontrol')
@@ -13,11 +13,11 @@ options(lime_password = 'L0912@tercy21')
 
 # Do stuff with LimeSurvey API
 get_session_key()  # Log in
-responses <- get_responses(731372, sResponseType = "short")  # Get results from survey
+responses <- get_responses(191945, sResponseType = "short")  # Get results from survey
 
 # Paramètres d'importation des données
 # raw_data <- call_limer(method = "export_responses", 
-#                        params = list(iSurveyID = 731372, 
+#                        params = list(iSurveyID = 191945, 
 #                                      sDocumentType = "csv", 
 #                                      sLanguageCode = NULL, 
 #                                      sCompletionStatus = "complete", 
@@ -38,6 +38,11 @@ d <- d %>%
   select(!c("lastpage","seed","startdate","submitdate",)) %>% 
   rename(lan = startlanguage, dat = datestamp)
 
+# Mise à jour light
+d <- d %>% 
+  select(!c("lastpage","seed","startdate","submitdate",)) %>% 
+  rename(lan = startlanguage, dat = datestamp)
+
 # Mise à jour du format des dates avec lubridate
 d$dat <- ymd_hms(d$dat)
 
@@ -46,3 +51,5 @@ d <- d %>%
 mutate(
   tim = case_when(dat >= as_date("01.01.2021", format="%d.%m.%Y") & dat <= as_date("21.01.2021", format="%d.%m.%Y") ~ "temps 1",
                   TRUE ~ "autre temps"))
+
+export(d,"sortie.xlsx")
