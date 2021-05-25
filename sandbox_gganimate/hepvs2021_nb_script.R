45642bc815fe73eae4be55ed0cc84632f7d51c5f
Nicolas Bressoud <65454893+bresnico@users.noreply.github.com>
2021-05-25 20:11:48 +0200

    Update hepvs2021_nb_script.R

--- sandbox_gganimate/hepvs2021_nb_script.R
+++ sandbox_gganimate/hepvs2021_nb_script.R
@@ -3,11 +3,26 @@
 # une ligne "réalisé"
 # Par domaine
 
+# Autre graphique du temps total par mois
+
 # Packages
+library(tidyverse)
+library(readxl)
 
 # Data
+d <- read_xlsx("../../data/hepvs2021_nb.xlsx", sheet = 2)
+e <-  read_xlsx("../../data/hepvs2021_nb.xlsx", sheet = 1)
+e <- e[1:15,]
 
+e <- e %>% 
+  rename(Domaine = `Domaine (1 % = 19 h)`,
+         Réalisé = `Réalisé (1 aout - 1 mai)`)
+  
 # Ggplot statique
+p <- e %>% 
+  ggplot() +
+  aes(x = Domaine, y = Planifié) +
+  geom_line()
 
 # gganimate
 
