6cffd6d11b43dddddcb4e09d30da5e329646214b
Nicolas Bressoud <65454893+bresnico@users.noreply.github.com>
2021-08-27 08:13:53 +0200

    prépa - traitement données erié élèves
    mise en forme du database et du rmd pour générer les graphs emo et cps (experimental)

--- hlm2021_nb/test.R
+++ hlm2021_nb/test.R
@@ -146,3 +146,58 @@ gt_table3 <-
             percent = "cell"
             ) %>%
   add_p()
+
+
+
+###############
+
+#Données enfants pris en charge (erie.xlsx)
+erie <- read_excel("erie.xlsx")
+
+# Création de la variable classe
+erie$classe <- str_extract(erie$code, "[a-z]+")
+
+# Création de la variable temps
+erie$temps <- str_sub(erie$code, start = -2L, end = -2L)
+
+# Création du score emo
+erie <- erie %>% 
+  mutate(emo5_5 = 6 - emo5_5) %>% 
+  mutate(emo_sco  = rowMeans(select(.,starts_with("emo")) ,na.rm =T))
+
+# Création du score cps
+erie <- erie %>% 
+  mutate(cps26_3 = 6 - cps26_3,
+         cps26_25 = 6 - cps26_25) %>% 
+  mutate(cps_sco  = rowMeans(select(.,starts_with("cps")) ,na.rm =T))
+
+pos <- position_jitter(width = 0.2, seed = 2)
+
+
+emo_vis <- erie %>% 
+  filter(classe== "a") %>%
+  filter(temps == "1") %>% 
+  ggplot() +
+  aes(x = classe, y = emo_sco, fill = classe, color = classe, alpha = 0.8) +
+  geom_boxplot(outlier.colour = NA) +
+  geom_jitter(position = pos, size = 5, alpha = .5, show.legend = FALSE) + 
+  geom_label_repel(position = pos, aes(label=id), size = 4, color = "black", max.overlaps = Inf) +
+  theme(legend.position='none') +
+  ggtitle("Visualisation") +
+  ylab("Score de régulation émotionnelle") +
+  xlab("élève(s) suivi(s)") +
+  theme(plot.title = element_text(hjust = 0.5))  
+
+# ERIE - élèves emo - variante temps all
+emo_vis_1_2 <- erie %>% 
+  filter(classe== "a") %>%
+  ggplot() +
+  aes(x = temps, y = emo_sco, fill = classe, color = classe, alpha = 0.8) +
+  geom_boxplot(outlier.colour = NA) +
+  geom_jitter(position = pos, size = 5, alpha = .5, show.legend = FALSE) + 
+  geom_label_repel(position = pos, aes(label=id), size = 4, color = "black", max.overlaps = Inf) +
+  theme(legend.position='none') +
+  ggtitle("Visualisation") +
+  ylab("Score de régulation émotionnelle") +
+  xlab("élève(s) suivi(s)") +
+  theme(plot.title = element_text(hjust = 0.5)) 
